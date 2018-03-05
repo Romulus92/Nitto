936a4c11255ec91badfb01709e49538e037979ba
Roman Zaynullin <r0mulusss@yandex.ru>
2018-03-05 15:52:03 +0500

    done with styling, added some interactivity

---
+++ build/assets/scripts/main.js
@@ -0,0 +1,46 @@
+$('.recommended__item').hover(
+    function() { $(this).addClass('recommended__item-activated') },
+    function() { $(this).removeClass('recommended__item-activated') }
+)
+
+$(".header-menu__item").click(function() {
+    $('.header-menu__item.header-menu__item-active').not(this).removeClass('header-menu__item-active');
+    $(this).toggleClass('header-menu__item-active');
+})
+
+jQuery(document).ready(function() {
+    // This button will increment the value
+    $('.qtyplus').click(function(e) {
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        fieldName = $(this).attr('field');
+        // Get its current value
+        var currentVal = parseInt($('input[name=' + fieldName + ']').val());
+        // If is not undefined
+        if (!isNaN(currentVal)) {
+            // Increment
+            $('input[name=' + fieldName + ']').val(currentVal + 1);
+        } else {
+            // Otherwise put a 0 there
+            $('input[name=' + fieldName + ']').val(0);
+        }
+    });
+    // This button will decrement the value till 0
+    $(".qtyminus").click(function(e) {
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        fieldName = $(this).attr('field');
+        // Get its current value
+        var currentVal = parseInt($('input[name=' + fieldName + ']').val());
+        // If it isn't undefined or its greater than 0
+        if (!isNaN(currentVal) && currentVal > 0) {
+            // Decrement one
+            $('input[name=' + fieldName + ']').val(currentVal - 1);
+        } else {
+            // Otherwise put a 0 there
+            $('input[name=' + fieldName + ']').val(0);
+        }
+    });
+});
