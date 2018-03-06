049de696997d7fc6b82010509104b74d23b3a656
Roman Zaynullin <r0mulusss@yandex.ru>
2018-03-06 17:43:48 +0500

    added fancybox pop-ups, custom scrollbar and few minor tweaks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,38 +8,41 @@ $(".header-menu__item").click(function() {
     $(this).toggleClass('header-menu__item-active');
 })
 
+$(".product-options__item").click(function() {
+    $('.product-options__item.product-options__item-selected').not(this).removeClass('product-options__item-selected');
+    $(this).toggleClass('product-options__item-selected');
+})
+
+$("[data-fancybox]").fancybox({
+    smallBtn: false
+});
+
+$(function() {
+    $('.close').on('click', e => {
+        e.preventDefault()
+        $.fancybox.close()
+    })
+})
+
 jQuery(document).ready(function() {
-    // This button will increment the value
     $('.qtyplus').click(function(e) {
-        // Stop acting like a button
         e.preventDefault();
-        // Get the field name
         fieldName = $(this).attr('field');
-        // Get its current value
         var currentVal = parseInt($('input[name=' + fieldName + ']').val());
-        // If is not undefined
         if (!isNaN(currentVal)) {
-            // Increment
             $('input[name=' + fieldName + ']').val(currentVal + 1);
         } else {
-            // Otherwise put a 0 there
             $('input[name=' + fieldName + ']').val(0);
         }
     });
-    // This button will decrement the value till 0
+
     $(".qtyminus").click(function(e) {
-        // Stop acting like a button
         e.preventDefault();
-        // Get the field name
         fieldName = $(this).attr('field');
-        // Get its current value
         var currentVal = parseInt($('input[name=' + fieldName + ']').val());
-        // If it isn't undefined or its greater than 0
         if (!isNaN(currentVal) && currentVal > 0) {
-            // Decrement one
             $('input[name=' + fieldName + ']').val(currentVal - 1);
         } else {
-            // Otherwise put a 0 there
             $('input[name=' + fieldName + ']').val(0);
         }
     });
